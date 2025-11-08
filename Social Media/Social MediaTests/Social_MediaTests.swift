2b3f2d15c5eb9a88be0e01d5b646172d13fae07f
Trungkun103 <trungkun103@gmail.com>
2025-11-08 15:17:05 +0700

    Upload Project

---
+++ Social Media/Social MediaTests/Social_MediaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Social_MediaTests.swift
+//  Social MediaTests
+//
+//  Created by TrungAnhx on 8/11/25.
+//
+
+import Testing
+@testable import Social_Media
+
+struct Social_MediaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
